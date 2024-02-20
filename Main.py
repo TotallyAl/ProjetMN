20d264bfb4be55261c08e77b6d03a4dfac5ce8d6
Alexandre Detienne <alexandredetienne@Alexandres-MBP.madhouse>
2024-02-20 20:41:08 +0100

    Question 1: Méthode de la sécante -> Récursive et itérative et Méthode de la bissection -> Récursive et itérative

--- Main.py
+++ Main.py
@@ -1,13 +1,17 @@
 #!/usr/bin/env python
-from RechercheRacine import bissection, secante
+from RechercheRacine import (
+    secante_iteration,
+    secante_recursive,
+    bissection_recursive,
+    bissection_iterative,
+)
 
 
 def f(x: float) -> float:
-    return x**2 - 3 * x + 1
+    return x**3 - 6 * x + 17
 
 
-solution: list = bissection(f, -5, 10, 0.00001)
-print(solution)
-
-
-# Changement
+solution: float
+statut: int
+print(bissection_recursive(f, -4, 3, 0.01))
+print(bissection_iterative(f, -4, 3, 0.01))
--- RechercheRacine.py
+++ RechercheRacine.py
@@ -1,33 +1,95 @@
-def bissection(f, x0: float, x1: float, tol: float) -> list:
-    """
-    Hypothèses initiales à remplir: -> f(x) doit etre continue sur l'intervalle [x0, x1]
-                                    -> f(x0) * f(x1) < 0
-                                    -> tol > 0
-    """
-    # Hypothèses 1: f(x) doit etre continue sur l'intervalle [x0, x1]
-    # Hypothèses 2: f(x0) * f(x1) < 0
-    if f(x0) * f(x1) > 0:
-        print("The selected interval is invalid for this algorithm.")
-        return
-    # Hypothèses 3: tol > 0
+from typing import Callable
+import numpy as np
+
+
+def secante_recursive(
+    f: Callable[[float], float], x0: float, x1: float, tol: float, max_iter: int = 100
+) -> list:
     if tol < 0:
-        print("Tol is negative or null.")
-        return
+        return [0, -1]
 
-    # Exécution de l'algorithme puisque les hypothèses sont remplies
+    iter = 0
+    f0 = f(x0)
+    while True:
+        f1 = f(x1)
+        if f0 == f1:
+            print("f0 et f1 ont la meme valeur")
+            return [0, -1]
+        x2 = x1 - (f1 * ((x1 - x0) / (f1 - f0)))
+        if np.abs(f(x2)) < tol:
+            return [x2, 0]
+        if iter > max_iter:
+            return [x2, -1]
+        iter += 1
+        x0 = x1
+        f0 = f1
+        x1 = x2
 
-    # Recherche du point se situant au milieu de l'intervalle
+
+def secante_iteration(
+    f: Callable[[float], float], x0: float, x1: float, tol: float, max_iter: int = 100
+) -> list:
+    if tol < 0:
+        return [0, -1]
+
+    iter = 0
+    f0 = f(x0)
+    while True:
+        f1 = f(x1)
+        if f0 == f1:
+            print("f0 et f1 ont la meme valeur")
+            return [0, -1]
+        x2 = x1 - (f1 * ((x1 - x0) / (f1 - f0)))
+        if np.abs(f(x2)) < tol:
+            return [x2, 0]
+        if iter > max_iter:
+            print("Reached max iteration")
+            return [x2, -1]
+        iter += 1
+        x0 = x1
+        f0 = f1
+        x1 = x2
+
+
+def bissection_recursive(
+    f: Callable[[float], float], x0: float, x1: float, tol: float, max_iter: int = 100
+) -> list:
+    if f(x0) * f(x1) > 0:
+        print("f(x0) et f(x1) sont de meme signes")
+        return [0, -1]
+    if tol < 0:
+        print("Tol is negatif of null")
+        return [0, -1]
+
     m = (x0 + x1) / 2
 
-    # Si le point au mileu de l'intervalle est plus petit que l'erreur, ce point est concidéré comme la solution
-    if m < tol:
-        return m
+    if np.abs(f(m)) < tol:
+        return [m, 0]
 
-    # On redéfinit l'intervalle de recherche pour la prochaine itération.
     if f(x0) * f(m) < 0:
-        return bissection(f, x0, m, tol)
-    return bissection(f, m, x1, tol)
+        return bissection_recursive(f, x0, m, tol)
+    return bissection_recursive(f, m, x1, tol)
 
 
-def secante(f, x0: float, x1: float, tol: float) -> list:
-    pass
+def bissection_iterative(
+    f: Callable[[float], float], x0: float, x1: float, tol: float, max_iter: int = 100
+) -> list:
+    if f(x0) * f(x1) > 0:
+        print("f(x0) et f(x1) sont de meme signes")
+        return [0, -1]
+    if tol < 0:
+        print("Tol is negatif of null")
+        return [0, -1]
+
+    iter = 0
+    while iter <= max_iter:
+        m = (x0 + x1) / 2
+        if np.abs(f(m)) < tol:
+            return [m, 0]
+        iter += 1
+        if f(x0) * f(m) < 0:
+            x1 = m
+        else:
+            x0 = m
+    print("Reached maximum amount of iterations")
+    return [0, -1]
